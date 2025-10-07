b9df2aa95b0fc5a6a71c17a26cdec85eac00c5c0
sally31 <111925455+sally31@users.noreply.github.com>
2025-10-07 19:50:31 +0100

    Add files via upload

---
+++ synergy_gen/blinky_thread_entry.c
@@ -0,0 +1,72 @@
+/***********************************************************************************************************************
+* File Name    : blinky_thread_entry.c
+* Description  : This is a very simple example application that blinks all the LEDs on a board.
+***********************************************************************************************************************/
+
+#include "blinky_thread.h"
+
+/*******************************************************************************************************************//**
+ * @brief  Blinky example application
+ *
+ * Blinks all leds at a rate of 1 second using the the threadx sleep function.
+ * Only references two other modules including the BSP, IOPORT.
+ *
+ **********************************************************************************************************************/
+void blinky_thread_entry(void)
+{
+    /* Define the units to be used with the threadx sleep function */
+	const uint32_t threadx_tick_rate_Hz = 100;
+    /* Set the blink frequency (must be <= threadx_tick_rate_Hz */
+    const uint32_t freq_in_hz = 10;
+    /* Calculate the delay in terms of the threadx tick rate */
+    const uint32_t delay = threadx_tick_rate_Hz/freq_in_hz;
+    /* LED type structure */
+    bsp_leds_t leds;
+    /* LED state variable */
+    ioport_level_t level = IOPORT_LEVEL_HIGH;
+
+    /* Get LED information for this board */
+    R_BSP_LedsGet(&leds);
+
+    /* If this board has no leds then trap here */
+    if (0 == leds.led_count)
+    {
+        while(1);   // There are no leds on this board
+    }
+
+    while (1)
+    {
+        UINT    err_g_queue_0;
+        char    rx_buf[4];
+        err_g_queue_0 = tx_queue_receive (&g_new_queue0, rx_buf, TX_NO_WAIT);
+        if (TX_SUCCESS == err_g_queue_0)
+        {
+            g_ioport.p_api->pinWrite(leds.p_leds[0], IOPORT_LEVEL_LOW);
+        }
+        else
+        {
+            g_ioport.p_api->pinWrite(leds.p_leds[0], IOPORT_LEVEL_HIGH);
+        }
+
+        tx_thread_sleep (delay);
+
+        /* Determine the next state of the LEDs */
+//        if(IOPORT_LEVEL_LOW == level)
+//        {
+//            level = IOPORT_LEVEL_HIGH;
+//        }
+//        else
+//        {
+//            level = IOPORT_LEVEL_LOW;
+//        }
+//
+//        /* Update all board LEDs */
+//        for(uint32_t i = 0; i < leds.led_count; i++)
+//        {
+//            //g_ioport.p_api->pinWrite(leds.p_leds[i], level);
+//        }
+//
+//        /* Delay */
+//        tx_thread_sleep (delay);
+    }
+}
